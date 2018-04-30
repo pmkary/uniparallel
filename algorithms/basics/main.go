9f91d21284d0cd1124b7e34610cf40ed9f7a87e0
pmkary <pouya@kary.us>
2018-04-30 15:25:34 +0430

    a basic exapmle

---
+++ algorithms/basics/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pmkary/parallel/tools"
+)
+
+// main
+func main() {
+	// ForAll is a loop that has starting and ending
+	// indexes, here that's from 2 to 10.
+	//
+	// Then it gets a grouping number which means how
+	// many threads (go routines) you want to use for
+	// this loop.
+	//
+	// At the end comes a function that takes an int
+	// for the index and inside of it you specify what
+	// you want as the body of your forloop to happen
+	tools.ForAll(2, 10, 4, func(index int) {
+		fmt.Println(index)
+	})
+}
