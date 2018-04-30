235187204e82bbacdd77dc6646084875469ce551
pmkary <pouya@kary.us>
2018-04-30 21:29:07 +0430

    pretty message

--- algorithms/basics/main.go
+++ algorithms/basics/main.go
@@ -18,6 +18,8 @@ func main() {
 	// At the end comes a function that takes an int
 	// for the index and inside of it you specify what
 	// you want as the body of your forloop to happen
+
+	fmt.Println("This ForAll prints indexes from 2 to 10")
 	tools.ForAll(2, 10, 4, func(index int) {
 		fmt.Println(index)
 	})
