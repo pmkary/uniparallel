d461fd823d640a13cdcb101894f1ff0a0d84e019
pmkary <pouya@kary.us>
2018-05-14 14:32:36 +0430

    readline for terminal

---
+++ tools/terminal.go
@@ -0,0 +1,17 @@
+package tools
+
+import (
+	"bufio"
+	"os"
+)
+
+// ReadLine reads from the terminal screen's
+// stream buffer and implements a read line
+func ReadLine() string {
+	reader := bufio.NewReader(os.Stdin)
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		return ""
+	}
+	return text
+}
